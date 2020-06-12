e8ff9d040213a333472ae785c3f8d81a471f8d89
Paulo <pauloh16.pdr@gmail.com>
2020-06-12 10:00:58 -0300

    Adicionar metodo para buscar post por id

---
+++ src/main/java/com/paulo/mongodb/service/PostService.java
@@ -0,0 +1,23 @@
+package com.paulo.mongodb.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulo.mongodb.domain.Post;
+import com.paulo.mongodb.repository.PostRepository;
+import com.paulo.mongodb.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService
+{
+	@Autowired
+	private PostRepository rep;
+
+	public Post findById(String id)
+	{
+		Optional<Post> post = rep.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
